530f29057b41b5c7ee430d3be2f1206d539e58c9
Kamil Balinski <102728966+BalinskiK@users.noreply.github.com>
2024-04-01 19:49:15 +0000

    asfgds

--- fs/read_write.c
+++ fs/read_write.c
@@ -616,18 +616,20 @@ ssize_t vfs_write(struct file *file, const char __user *buf, size_t count, loff_
 	file_start_write(file);
 	if (file->f_op->write)
 		ret = file->f_op->write(file, buf, count, pos);
-	else if (file->f_op->write_iter)
+	else if (file->f_op->write_iter){
 		ret = new_sync_write(file, buf, count, pos);
+
 		if (file->f_path.dentry) {
 			struct dentry *dentry = file->f_path.dentry;
 			char xattr_value[33]; // Adjust the size as needed
 
 			ssize_t len = vfs_getxattr(&init_user_ns, dentry, "user.cw3_hide", xattr_value, sizeof(xattr_value));
 
-			if (len >= 0) {
+			if (len >= 0){
 				printk("true");
 			}
 		}
+	}
 	else
 		ret = -EINVAL;
 	if (ret > 0) {
